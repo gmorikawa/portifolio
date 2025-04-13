7eb813a0e52695f2a5f792273d493760facb49da
gmorikawa <morikawa.gabriel@outlook.jp>
2025-04-13 18:26:34 +0900

    Setup with Nextjs

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,32 @@
+export default function Contact() {
+    return (
+        <main id="content">
+            <section id="business-card" className="card">
+                <header>
+                    <h2 className="card-title">Gabriel Morikawa</h2>
+                    <h3 className="card-subtitle">Software Developer</h3>
+                </header>
+
+                <footer>
+                    <ul className="list list-icon-text">
+                        <li className="list-item">
+                            <img src="https://s.magecdn.com/social/mb-mail.svg" width="13px" height="13px" />
+                                <p>gabriel.morikawa@live.com</p>
+                        </li>
+
+                        <li className="list-item">
+                            <img src="https://s.magecdn.com/social/tc-linkedin.svg" width="13px" height="13px" />
+                                <p>linkedin.com/in/gmorikawa</p>
+                        </li>
+
+                        <li className="list-item">
+                            <img src="https://cdn.simpleicons.org/github/181717" width="13px" height="13px" />
+                                <p>github.com/gmorikawa</p>
+                        </li>
+                    </ul>
+                </footer>
+            </section>
+        </main>
+    );
+}
+
---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+export default function Home() {
+    return (
+        <main id="content">
+            <section id="profile-card" className="card">
+                <header>
+                    <h2 className="card-title">Gabriel Morikawa</h2>
+                </header>
+
+                <section>
+                    <header>
+                        <h4>Who I am?</h4>
+                    </header>
+
+                    <main>
+                        <p>
+                            Software Developer with focus in developing scalable web application.
+                            I have a Associate's Degree on Game Development at Fatec de Lins - Prof. Antonio Seabra.
+                        </p>
+                    </main>
+                </section>
+            </section>
+        </main>
+    );
+}
