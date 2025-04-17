37622a8c618f8b6707d4f840111b203b8aa39e17
gmorikawa <morikawa.gabriel@outlook.jp>
2025-04-17 22:52:18 +0900

    Added more detailed information about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,31 @@
+import Image from "next/image";
+
 export default function Home() {
     return (
         <main id="content">
             <section id="profile-card" className="card">
-                <header>
-                    <h2 className="card-title">Gabriel Morikawa</h2>
+                <header className="mb-6">
+                    <h2 className="card-title">About me</h2>
                 </header>
 
-                <section>
-                    <header>
-                        <h4>Who I am?</h4>
-                    </header>
+                <main>
+                    <p className="mb-4">
+                        Hello, my name is Gabriel Morikawa! Nice to meet you!
+                    </p>
 
-                    <main>
-                        <p>
-                            Software Developer with focus in developing scalable web application.
-                            I have a Associate&apos;s Degree on Game Development at Fatec de Lins - Prof. Antonio Seabra.
-                        </p>
-                    </main>
-                </section>
+                    <p className="mb-4">
+                        I am a software engineer very passionate about coding and technology. Professionaly, I have mainly experience in full-stack web development, but I also have experience in game and mobile development. During my journey I'd used many programming languages, databases, frameworks, tools, all just out of curiosity.
+                    </p>
+
+                    <p className="mb-4">
+                        My passion about coding started when I first participated on a competitive programming contest organized locally by my university. It was so fun to compete trying to find a correct and optimized algorithm that would pass all tests cases.
+                    </p>
+
+                    <p className="mb-4">
+                        I am always looking for new challenges, so if you have any interesting project in mind, feel free to reach out!
+                    </p>
+                </main>
             </section>
-        </main>
+        </main >
     );
 }
